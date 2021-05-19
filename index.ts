0209fce5243afa859f1a0557efc2770bb5d030f7
Nihinlola Fajemilehin <nihinfaj@gmail.com>
2021-05-19 19:28:04 +0100

    added comments

--- index.ts
+++ index.ts
@@ -3,14 +3,17 @@ const Crypto = require('crypto');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    // Generate secret signature in Azure Functions
     const hmac = Crypto.createHmac("sha1", "hmXkVFHI7fSCcjAZpskJ/jc4hjata14i7dKtMKDHbUveki1pCyaAiA==");
     const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
     const shaSignature = `sha1=${signature}`;
 
+    // Extract secret signature from GitHub Webhooks Request Header
     const gitHubSignature = req.headers['x-hub-signature'];
 
+    // Compare secret signatures, if the same process request
     if (!shaSignature.localeCompare(gitHubSignature)) {
-        // Existing code
+        // If request contains title, continue, else return error message
         if (req.body.pages[0].title) {
         context.res = {
             body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
@@ -23,6 +26,7 @@ module.exports = async function (context, req) {
                 }
             }
     }
+    // If secret signatures different, return error message
     else {
     context.res = {
         status: 401,
